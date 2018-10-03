33cf3136c1ecb3eb29886e3b5eee999e0d03bcfe
Kevin Chen <kevinmichaelchen@gmail.com>
2018-10-03 17:08:55 -0400

    Set fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-
 import Layout from '../components/layout'
+import font from './font.css'
 
 const renderPost = (post, index) => {
   return (
